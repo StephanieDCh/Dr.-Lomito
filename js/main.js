df117d581a13ec72f36adecc56261242820519be
ChristianMendoza <onlyespron@gmail.com>
2022-09-12 16:36:28 -0500

    Validación lowerCase

--- js/main.js
+++ js/main.js
@@ -9,19 +9,19 @@ let correo = document.getElementById("correo");
     for (let i = 0; i < correo.value.length; i++) {
     console.log((!isNaN(correo.value.charAt(i))) );
     console.log(correo.value.charAt(i));
-    console.log(correo.value.toUpperCase().charCodeAt(i));
+    console.log(correo.value.toLowerCase().charCodeAt(i));
     if(  (
-             ( correo.value.toUpperCase().charCodeAt(i)<65)
+             ( correo.value.toLowerCase().charCodeAt(i)<65)
              ||
-             (correo.value.toUpperCase().charCodeAt(i)>90)
+             (correo.value.toLowerCase().charCodeAt(i)>90)
         )
-        && ((correo.value.toUpperCase().charCodeAt(i)!=32) ) // espacio
-        && ((correo.value.toUpperCase().charCodeAt(i)!=193) ) // Á
-        && ((correo.value.toUpperCase().charCodeAt(i)!=201) ) // É
-        && ((correo.value.toUpperCase().charCodeAt(i)!=205) ) // Í
-        && ((correo.value.toUpperCase().charCodeAt(i)!=211) ) // Ó
-        && ((correo.value.toUpperCase().charCodeAt(i)!=218) ) // Ú
-        && ((correo.value.toUpperCase().charCodeAt(i)!=209) ) // Ñ
+        && ((correo.value.toLowerCase().charCodeAt(i)!=32) ) // espacio
+        && ((correo.value.toLowerCase().charCodeAt(i)!=193) ) // Á
+        && ((correo.value.toLowerCase().charCodeAt(i)!=201) ) // É
+        && ((correo.value.toLowerCase().charCodeAt(i)!=205) ) // Í
+        && ((correo.value.toLowerCase().charCodeAt(i)!=211) ) // Ó
+        && ((correo.value.toLowerCase().charCodeAt(i)!=218) ) // Ú
+        && ((correo.value.toLowerCase().charCodeAt(i)!=209) ) // Ñ
    ) {
         correo.classList.remove("is-valid");
         correo.classList.add("is-invalid");
