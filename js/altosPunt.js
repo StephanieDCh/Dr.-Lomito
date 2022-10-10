b442e740f3624edda6f5d2138bde472b2dabbfea
Josue Chavez <josue.chavezlomeli@gmail.com>
2022-10-09 20:38:47 -0500

    Se crean cards para index

--- js/altosPunt.js
+++ js/altosPunt.js
@@ -1,82 +1,47 @@
-// let puntajeCard = [
-//     {"id":1, puntajes, img:'',nombre:'Juan Pérez',descripcion:'Especialista en Medicina y Cirugía en Perros y Gatos',calificacion:'5 estrellas'},
-//     {"id":2, puntajes, img:'',nombre:'Josué Chávez',descripcion:'Especialista en Medicina y Cirugía en Perros y Gatos',calificacion:'5 estrellas'},
-//     {"id":3, puntajes, img:'',nombre:'Juan Carlos Rojas',descripcion:'Especialista en Medicina y Cirugía en Perros y Gatos',calificacion:'5 estrellas'}
-// ];
-// let cards = " ";
+let listCards = [];
+let cardIndex = document.getElementById("cardIndex");
 
-// puntajeCard.forEach((item) => {
-//     cards += `<div class="card-deck">
-//     <div class="card" id="puntaje1">
-//       <img class="card-img-top" src="${item.img}" alt="Card image cap">
-//       <div class="card-body">
-//       <h5 class="card-title">${item.nombre}</h5>
-// id        <p class="card-text">${item.descripcion.slice(0, 100)}</p>
-//         <p class="card-text"><small class="text-muted">${item.calificacion}</small></p>
-//         <a href="#" class="btn btn-dark">Agendar</a>
-//       </div>
-//     </div>
-//     `;
-// });
+window.addEventListener("load", function(e){
+e.preventDefault();
 
-// const cardPuntajes = document.getElementById("cardPuntajes");
-// cardPuntajes.innerHTML = cards;
-// const vetCard = document.getElementById("vetCard");
+if(localStorage.getItem("users")){
+listCards = JSON.parse(localStorage.getItem("users"));
 
-// function vetCard(id) {
-//     vetCard.innerHTML = " ";
-//     vetCard.style.transform = "scale(1)";
-//     vetCard.style.opacity = 1;
-//     cardPuntajes.style.opacity = 0.5;
-    
-//     puntajeCard.forEach((item) => {
-//     if (item.id == id){
-//         let profile_div = `<div class="card-deck">
-//         <div class="card" id="puntaje1">
-//           <img class="card-img-top" src="${item.img}" alt="Card image cap">
-//           <div class="card-body">
-//           <h5 class="card-title">${item.nombre}</h5>
-//     id        <p class="card-text">${item.descripcion.slice(0, 100)}</p>
-//             <p class="card-text"><small class="text-muted">${item.calificacion}</small></p>
-//             <a href="#" class="btn btn-dark">Agendar</a>
-//           </div>
-//         </div>
-//         `;
-//     }
-// }
-//     )};
-
-const puntajes = document.querySelector("puntajes");
-function createCard (img,nombre,descripcion,calificacion) {
-    let puntajeCard = `<div class="card-deck">
-    <div class="card" id="puntaje1">
-      <img class="card-img-top" src="${img}" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">${nombre}</h5>
-        <p class="card-text">${descripcion}</p>
-        <p class="card-text"><small class="text-muted">${calificacion}</small></p>
-        <a href="#" class="btn btn-dark">Agendar</a>
+for (let i = 0; i < 2; i++) {
+  let item = listCards[i];
+  cardIndex.innerHTML += `
+  <div class="card  all col-md-5 m-1">
+      <div class="presentacion">
+           <div class="img">
+              <img id="imagenTest" src="${item.img}" height="300px" class="card-img-top"  alt="...">
+          </div>
+          <div class="producto-info">
+              <div class="producto-texto">
+              
+              <h1 class="card-title">${item.nombre}</h1>
+              <p class="card-text">
+              <i class="fa fa-paw " style="color:orange; text-shadow: 0 0 3px #000;" id="pata">
+              <i class="fa fa-paw " style="color:orange; text-shadow: 0 0 3px #000;" id="pata"></i>
+              <i class="fa fa-paw " style="color:orange; text-shadow: 0 0 3px #000;" id="pata"></i>
+              <i class="fa fa-paw " style="color:orange; text-shadow: 0 0 3px #000;" id="pata"></i>
+              <i class="fa fa-paw " style="color:orange; text-shadow: 0 0 3px #000;" id="pata"></i>
+              </i></p>
+              <br>
+              <h2 class="card-text">${item.especialidad}</h2>
+              <p class="card-text">${item.descripcion}</p>
+              <br>
+              <br>
+              <br>
+              <p class="card-text">Consulta General<strong> $${item.costoConsulta}.00 MXN</strong></p>
+              
+              <br>
+              <div class="precio-btn">
+              <a href="../pages/doctorVerMas.html"type="buton" class="btn btn-dark" id="btnVerMas_${i}">Ver más</a>
+          </div>
       </div>
-    </div>
-    `;
-    puntajes.innerHTML += puntajeCard;
+  </div>
+  `;
+  
 }
-
-    let puntaje1 = ["",
-    "Juan Pérez", 
-    "Especialista en Medicina y Cirugía en Perros y Gatos",
-    "5 estrellas",];
-
-    let puntaje2 = ["",
-    "Josué Chávez", 
-    "Especialista en Medicina y Cirugía en Perros y Gatos",
-    "5 estrellas",];
-
-    let puntaje3 = ["",
-    "Juan Carlos Rojas", 
-    "Especialista en Medicina y Cirugía en Perros y Gatos",
-    "5 estrellas",];
-
-createCard(puntaje2);
-
-createCard(puntaje3);
+}
+});
