4318ece8306f0e98f51d413997a3018af0ddaa25
Josue Chavez <josue.chavezlomeli@gmail.com>
2022-10-14 00:54:16 -0500

    Se actualizan archivos SQLscript para creacion e inserts de base de datos

--- BaseDeDatos/drlomito_CREATEv4.sql
+++ BaseDeDatos/drlomito_CREATEv4.sql
@@ -21,7 +21,7 @@ USE `drlomito` ;
 -- Table `drlomito`.`categoria_vet`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `drlomito`.`categoria_vet` (
-  `categoria_id` INT NOT NULL AUTO_INCREMENT,
+  `categoria_id` BIGINT NOT NULL AUTO_INCREMENT,
   `categoria_nombre` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`categoria_id`))
 ENGINE = InnoDB
@@ -34,7 +34,7 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `drlomito`.`tipo_usuario`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `drlomito`.`tipo_usuario` (
-  `tipo_usuario_id` INT NOT NULL AUTO_INCREMENT,
+  `tipo_usuario_id` BIGINT NOT NULL AUTO_INCREMENT,
   `tipo_usuario_nombre` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`tipo_usuario_id`))
 ENGINE = InnoDB
@@ -47,12 +47,12 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `drlomito`.`usuarios`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `drlomito`.`usuarios` (
-  `usuario_id` INT NOT NULL AUTO_INCREMENT,
+  `usuario_id` BIGINT NOT NULL AUTO_INCREMENT,
   `usuario_nombre` VARCHAR(50) NOT NULL,
   `usuario_correo` VARCHAR(60) NOT NULL,
   `usuario_contrasena` VARCHAR(15) NOT NULL,
-  `veterinario_imagen` LONGBLOB NULL DEFAULT NULL,
-  `tipo_usuario_tipo_usuario_id` INT NOT NULL,
+  `veterinario_urlimagen` VARCHAR(400) NULL DEFAULT NULL,
+  `tipo_usuario_tipo_usuario_id` BIGINT NOT NULL,
   PRIMARY KEY (`usuario_id`, `tipo_usuario_tipo_usuario_id`),
   INDEX `fk_usuarios_tipo_usuario1_idx` (`tipo_usuario_tipo_usuario_id` ASC) VISIBLE)
 ENGINE = InnoDB
@@ -65,14 +65,14 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `drlomito`.`contacto_vet`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `drlomito`.`contacto_vet` (
-  `contacto_id` INT NOT NULL AUTO_INCREMENT,
+  `contacto_id` BIGINT NOT NULL AUTO_INCREMENT,
   `veterinario_direccion` VARCHAR(100) NOT NULL,
   `veterinario_telefono1` VARCHAR(10) NOT NULL,
   `veterinario_telefono2` VARCHAR(10) NOT NULL,
   `veterinario_horario_inicio` INT NOT NULL,
   `veterinario_horario_cierre` INT NOT NULL,
-  `veterinario_atencion_urgencias` TINYINT NOT NULL,
-  `usuarios_usuario_id` INT NOT NULL,
+  `veterinario_atencion_urgencias` BIT(1) NOT NULL,
+  `usuarios_usuario_id` BIGINT NOT NULL,
   PRIMARY KEY (`contacto_id`, `usuarios_usuario_id`),
   INDEX `fk_contacto_vet_usuarios1_idx` (`usuarios_usuario_id` ASC) VISIBLE)
 ENGINE = InnoDB
@@ -85,14 +85,14 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `drlomito`.`info_veterinario`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `drlomito`.`info_veterinario` (
-  `info_veterinario_id` INT NOT NULL AUTO_INCREMENT,
+  `info_veterinario_id` BIGINT NOT NULL AUTO_INCREMENT,
   `veterinario_especialidad` VARCHAR(100) NOT NULL,
   `veterinario_calificacion` INT NOT NULL,
   `veterinario_descripcion` VARCHAR(700) NOT NULL,
-  `veterinario_costo_consulta` DECIMAL(8,2) NOT NULL,
+  `veterinario_costo_consulta` DOUBLE NOT NULL,
   `veterinario_servicios` VARCHAR(300) NOT NULL,
-  `categoria_vet_categoria_id` INT NOT NULL,
-  `usuarios_usuario_id` INT NOT NULL,
+  `categoria_vet_categoria_id` BIGINT NOT NULL,
+  `usuarios_usuario_id` BIGINT NOT NULL,
   PRIMARY KEY (`info_veterinario_id`, `categoria_vet_categoria_id`, `usuarios_usuario_id`),
   INDEX `fk_info_veterinario_categoria_vet_idx` (`categoria_vet_categoria_id` ASC) VISIBLE,
   INDEX `fk_info_veterinario_usuarios1_idx` (`usuarios_usuario_id` ASC) VISIBLE)
@@ -106,11 +106,11 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `drlomito`.`opiniones_vet`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `drlomito`.`opiniones_vet` (
-  `opiniones_id` INT NOT NULL AUTO_INCREMENT,
+  `opiniones_id` BIGINT NOT NULL AUTO_INCREMENT,
   `opiniones_nombre_autor` VARCHAR(50) NOT NULL,
   `opiniones_calificacion` INT NOT NULL,
   `opiniones_comentario` VARCHAR(300) NOT NULL,
-  `usuarios_usuario_id` INT NOT NULL,
+  `usuarios_usuario_id` BIGINT NOT NULL,
   PRIMARY KEY (`opiniones_id`, `usuarios_usuario_id`),
   INDEX `fk_opiniones_vet_usuarios1_idx` (`usuarios_usuario_id` ASC) VISIBLE)
 ENGINE = InnoDB
